bc6c1caf6960e112b7087f554eb771f13c4c41e2
Diana Lopez <me@diana.nu>
2020-09-17 14:17:38 -0700

    initial commit

---
+++ plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: Cuttlesoft Blurb
+ * Description: Header and a paragraph.
+ * Author: Diana Lopez
+ * Version: 1.0.0
+ *
+ * @package CGB
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block Initializer.
+ */
+require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
